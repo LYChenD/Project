3711bfadc7671d41e4a36fe3b57e4a060bb482dc
LYChenD <36179725+LYChenD@users.noreply.github.com>
2018-02-27 11:54:11 +0800

    Update wechat.sql

--- wechat.sql
+++ wechat.sql
@@ -9,6 +9,6 @@ CREATE TABLE dialogue (
   answer TEXT NOT NULL COMMENT '回答'
 ) COMMENT '聊天表';
 
-INSERT INTO dialogue(id, question, answer) VALUES (NULL,'肉丝，嫁给我吧', '我怕我配不上你，我曾经有一段刻骨铭心的惨痛经历。'),
+INSERT INTO dialogue(id, question, answer) VALUES (NULL,'嫁给我吧', '我怕我配不上你，我曾经有一段刻骨铭心的惨痛经历。'),
                                                   (NULL, '说出来吧，不管是什么，我都能接受', '我爱杰克'),
                                                   (NULL, '接的多吗', '滚球！！！！！！！');
